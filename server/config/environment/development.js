35aa3c78b17362b19d039724f1fa5a535776c1a8
Yosuke Tamura <tamura.yosuke.tp8@gmail.com>
2015-06-21 15:52:49 +0900

    fix database uri

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/qiitenai-dev'
+    uri: MONGOLAB_URI || 'mongodb://localhost/qiitenai-dev'
   },
 
   seedDB: true
